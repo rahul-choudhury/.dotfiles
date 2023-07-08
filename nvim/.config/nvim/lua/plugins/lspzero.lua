c1dee2c64632347e05e4e021a20a1fb7dcb417c7
Rahul Choudhury <rchoudhury63@gmail.com>
2023-07-08 22:14:07 +0530

    fix(nvim): remove lspzero lazy loading

--- nvim/.config/nvim/lua/plugins/lspzero.lua
+++ nvim/.config/nvim/lua/plugins/lspzero.lua
@@ -1,92 +1,58 @@
 return {
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        lazy = true,
-        config = function()
-            require('lsp-zero.settings').preset({})
-        end
-    },
-
-    -- Autocompletion
-    {
-        'hrsh7th/nvim-cmp',
-        event = 'InsertEnter',
-        dependencies = {
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-nvim-lua',
-            'hrsh7th/cmp-buffer',
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
-            'rafamadriz/friendly-snippets',
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},             -- Required
+        {                                      -- Optional
+            'williamboman/mason.nvim',
+            build = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
         },
-        config = function()
-            require('lsp-zero.cmp').extend()
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-            local cmp = require('cmp')
-            local cmp_action = require('lsp-zero.cmp').action()
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
 
-            require('luasnip.loaders.from_vscode').lazy_load()
-
-            cmp.setup({
-                sources = {
-                    { name = 'path' },
-                    { name = 'nvim_lsp' },
-                    { name = 'nvim_lua' },
-                    { name = 'buffer' },
-                    { name = 'luasnip' },
-                },
-
-                mapping = {
-                    ['<CR>'] = cmp.mapping.confirm({select = false}),
-                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-                }
-            })
-        end
+        {'hrsh7th/cmp-path'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'hrsh7th/cmp-buffer'},
+        {'saadparwaiz1/cmp_luasnip'},
+        {'rafamadriz/friendly-snippets'},
     },
+    config = function ()
+        local lsp = require('lsp-zero').preset({})
 
-    -- LSP
-    {
-        'neovim/nvim-lspconfig',
-        cmd = 'LspInfo',
-        event = {'BufReadPre', 'BufNewFile'},
-        dependencies = {
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'williamboman/mason-lspconfig.nvim'},
-            {
-                'williamboman/mason.nvim',
-                build = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
+        lsp.on_attach(function(client, bufnr)
+            lsp.default_keymaps({buffer = bufnr})
+        end)
+
+        -- (Optional) Configure lua language server for neovim
+        require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+        lsp.setup()
+
+        -- You need to setup `cmp` after lsp-zero
+        local cmp = require('cmp')
+        local cmp_action = require('lsp-zero').cmp_action()
+
+        require('luasnip.loaders.from_vscode').lazy_load()
+
+        cmp.setup({
+            sources = {
+                {name = 'path'},
+                {name = 'nvim_lsp'},
+                {name = 'buffer'},
+                {name = 'luasnip'},
             },
-        },
-        config = function()
-            require('mason').setup({
-                ui = {
-                    icons = {
-                        package_installed = "●",
-                        package_pending = "○",
-                        package_uninstalled = "○",
-                    },
-                }
-            })
-
-            local lsp = require('lsp-zero')
-
-            lsp.on_attach(function(client, bufnr)
-                lsp.default_keymaps({buffer = bufnr})
-            end)
-
-            lsp.ensure_installed({
-                'lua_ls',
-                'tsserver',
-            })
-
-            require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
-            lsp.setup()
-        end
-    }
+            mapping = {
+                ['<CR>'] = cmp.mapping.confirm({select = false}),
+                ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+                ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+            }
+        })
+    end
 }
