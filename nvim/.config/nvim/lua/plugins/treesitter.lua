e2b6778f570015bc8ff05ed294468df8c7c84cd4
Rahul Choudhury <rchoudhury63@gmail.com>
2023-07-08 22:18:47 +0530

    feat(nvim): add nvim-ts-context-commentstring

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,17 +3,22 @@ return {
     dependencies = {
         'nvim-treesitter/nvim-treesitter-textobjects',
         'nvim-treesitter/nvim-treesitter-context',
+        'JoosepAlviste/nvim-ts-context-commentstring',
     },
     build = ":TSUpdate",
     config = function ()
         require'nvim-treesitter.configs'.setup {
-            ensure_installed = { 'javascript', 'lua', 'tsx', 'typescript', 'vim', 'vimdoc' },
+            ensure_installed = { 'css', 'html', 'javascript', 'lua', 'tsx', 'typescript', 'vim', 'vimdoc' },
 
             sync_install = false,
             auto_install = true,
 
             highlight = { enable = true },
             indent = { enable = true },
+            context_commentstring = {
+                enable = true,
+                enable_autocmd = false,
+            },
         }
     end
 }
